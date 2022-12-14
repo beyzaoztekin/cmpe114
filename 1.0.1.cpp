6d5e77e8ed874a9e7edd162fb073a56783b42341
Beyza Öztekin <120556675+beyzaoztekin@users.noreply.github.com>
2022-12-14 14:09:11 +0300

    fonksiyonlu2

---
+++ 1.0.1.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+void reverse (int size, int a[]);
+
+int main()
+{
+    int x[100], n, i;
+
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+    printf("Enter array elements: ");
+    for (i = 0; i < n; i++)
+        scanf("%d", &x[i]);
+    reverse(n, x);
+    return 0;
+    
+}
+
+void reverse(int size, int arr[])
+{
+    int i, temp;
+    int mid = size / 2;
+
+    for (i = 0; i < mid; i++)
+    {
+        temp = arr[i];
+        arr[i] = arr[size-i-1];
+        arr[size-i-1] = temp;
+    }
+    printf("Reversed array: ");
+    for (i = 0; i < n; i++)
+        printf("%4d", x[i]);
+}
